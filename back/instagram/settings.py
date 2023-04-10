d971eba2a923914154f7c1784e949ba0bb8627dd
한원종 <gksdnjswhd10@naver.com>
2023-04-10 17:53:26 +0900

    django

--- back/instagram/settings.py
+++ back/instagram/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'djangorestframework',
+    'django_bootstrap_v5',
 ]
 
 MIDDLEWARE = [
